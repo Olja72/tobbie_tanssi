a50eb97a077872fb4eb360572f05cf5cbb0b9085
Olja72 <olja.anttonen@gmail.com>
2025-08-08 14:04:41 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,17 +11,5 @@ TobbieII.rightward()
 basic.pause(500)
 TobbieII.shake_head(10)
 basic.pause(500)
-TobbieII.stopwalk()
 basic.showIcon(IconNames.Heart)
 basic.showString("HELLO, ELIAS!")
-music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Birthday), music.PlaybackMode.UntilDone)
-basic.forever(function () {
-    if (TobbieII.RBlock(300)) {
-        TobbieII.leftward()
-    } else if (TobbieII.LBlock(300)) {
-        TobbieII.rightward()
-        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.BaDing), music.PlaybackMode.InBackground)
-    } else {
-        TobbieII.forward()
-    }
-})
